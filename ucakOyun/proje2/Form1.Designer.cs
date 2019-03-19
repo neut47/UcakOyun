356ed18cdf71bcb85d8dc74f75b138fb0e34b4fb
neut47 <48730433+neut47@users.noreply.github.com>
2019-03-19 21:06:52 +0300

    Add files via upload

---
+++ ucakOyun/proje2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace proje2
+{
+    partial class UcakOyun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UcakOyun));
+            this.ucak = new System.Windows.Forms.PictureBox();
+            this.mHarZmn = new System.Windows.Forms.Timer(this.components);
+            this.dHarZmn = new System.Windows.Forms.Timer(this.components);
+            this.dOlZmn = new System.Windows.Forms.Timer(this.components);
+            this.yokEtZmn = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ucak)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ucak
+            // 
+            this.ucak.Image = ((System.Drawing.Image)(resources.GetObject("ucak.Image")));
+            this.ucak.Location = new System.Drawing.Point(0, 421);
+            this.ucak.Name = "ucak";
+            this.ucak.Size = new System.Drawing.Size(40, 41);
+            this.ucak.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ucak.TabIndex = 0;
+            this.ucak.TabStop = false;
+            this.ucak.Click += new System.EventHandler(this.ucak_Click);
+            // 
+            // mHarZmn
+            // 
+            this.mHarZmn.Interval = 10;
+            this.mHarZmn.Tick += new System.EventHandler(this.mHarZmn_Tick);
+            // 
+            // dHarZmn
+            // 
+            this.dHarZmn.Interval = 50;
+            this.dHarZmn.Tick += new System.EventHandler(this.dHarZmn_Tick);
+            // 
+            // dOlZmn
+            // 
+            this.dOlZmn.Interval = 1000;
+            this.dOlZmn.Tick += new System.EventHandler(this.dOlZmn_Tick);
+            // 
+            // yokEtZmn
+            // 
+            this.yokEtZmn.Interval = 10;
+            this.yokEtZmn.Tick += new System.EventHandler(this.yokEtZmn_Tick);
+            // 
+            // UcakOyun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(322, 461);
+            this.Controls.Add(this.ucak);
+            this.MaximizeBox = false;
+            this.Name = "UcakOyun";
+            this.Text = "UcakOyun";
+            this.Load += new System.EventHandler(this.UcakOyun_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ucak)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ucak;
+        private System.Windows.Forms.Timer mHarZmn;
+        private System.Windows.Forms.Timer dHarZmn;
+        private System.Windows.Forms.Timer dOlZmn;
+        private System.Windows.Forms.Timer yokEtZmn;
+    }
+}
+
---
+++ ucakOyun/proje2/Form1.cs
@@ -0,0 +1,197 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace proje2
+{
+    public partial class UcakOyun : Form
+    {
+        // Düşman ve mermi tanımlandı       
+        Image resDusman = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "dusman.png");
+        Image resMermi = Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "mermi.png");
+
+        public UcakOyun()
+        {
+            InitializeComponent();
+        }
+
+        private void ucak_Click(object sender, EventArgs e)
+        { }
+
+        private void UcakOyun_Load(object sender, EventArgs e)
+        {
+            //Klavyeyi algılaması, düsmanların olsuması ve düsmanların yokedilmesi, ayrıca ekranı kilitleme
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            dOlZmn.Start();
+            yokEtZmn.Start();
+            this.KeyDown += new KeyEventHandler(UcakOyun_KeyDown);
+        }
+
+        // Klavye tuşlarının ayarlandığı yer
+        private void UcakOyun_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = ucak.Location.X;
+
+            if (e.KeyCode == Keys.Right) x += 20;
+
+            else if (e.KeyCode == Keys.Left) x -= 20;
+
+
+            if (x > 280)
+                ucak.Location = new Point(280, 421);
+            else if (x < 0)
+                ucak.Location = new Point(0, 421);
+            else
+                ucak.Location = new Point(x, 421);
+            //merminin oluşturulduğu yer. resmi, boyutu, hızı
+            if (e.KeyCode == Keys.Space)
+            {
+                PictureBox mermi = new PictureBox();
+                mermi.BackgroundImage = resMermi;
+                mermi.SetBounds(ucak.Location.X + 17, ucak.Location.Y - 20, 8, 12);
+                this.Controls.Add(mermi);
+                mermi.Visible = true;
+                mHarZmn.Start();
+            }
+        }
+
+        // merminin nasıl gideceğinin ayarlandığı yer
+        private void mHarZmn_Tick(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                PictureBox mermi = c as PictureBox;
+                if (mermi.Height == 12)
+                {
+                    if (mermi.Location.Y > 0)
+                    {
+                        mermi.SetBounds(mermi.Location.X, mermi.Location.Y - 3, 8, 12);
+                    }
+                    else
+                    {
+                        mermi.Dispose();
+                    }
+                }
+            }
+        }
+
+        //Düşman bilgileri yer alıyor. Rastgele oluşumu, resmi, boyutu, hareket zamanlayıcısı
+        public void dusmanOlustur()
+        {
+            Random rand = new Random();
+            PictureBox dusman = new PictureBox();            
+            int konumX = rand.Next(0, 294);
+            dusman.BackgroundImage = resDusman;
+            dusman.BackgroundImageLayout = ImageLayout.Zoom;
+            dusman.SetBounds(konumX, 5, 40, 40);
+            this.Controls.Add(dusman);
+            dusman.Visible = true;
+            dHarZmn.Start();
+        }
+
+        // Düşmanın nasıl gideceğinin ayarlandığı yer
+        private void dHarZmn_Tick(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                PictureBox dusman = c as PictureBox;
+                if (dusman.Height == 40)
+                {
+                    // Düşman belirlenen koordinatlara geldiğinde oyun sona erer
+                    if (dusman.Location.Y == ucak.Location.Y + 1)
+                    {
+                        dHarZmn.Stop();
+                        dOlZmn.Stop();
+                        mHarZmn.Stop();
+                        //mesaj kutusu oluşturur.                        
+                        DialogResult dr = MessageBox.Show("Devam etmek istiyor musunuz?", "Oyun Bitti!!!", MessageBoxButtons.YesNo);
+                        //yeni form tanımlar. formu gösterir. son form dısındakileri kaldırır
+                        if (dr == DialogResult.Yes)
+                        {
+                            UcakOyun form = new UcakOyun();
+                            form.Show();
+                            this.Dispose(false);
+                        }
+                        else
+                            Application.Exit();
+                    }
+                    // Düşman hareketini ayarlar
+                    dusman.SetBounds(dusman.Location.X, dusman.Location.Y + 3, 40, 40);
+                }
+            }
+        }
+
+        //Düşmanın oluşturulma sıklığı için zamanlayıcı      
+        private void dOlZmn_Tick(object sender, EventArgs e)
+        {
+            dusmanOlustur();
+        }        
+
+        // İmha zamanlayıcısı
+        private void yokEtZmn_Tick(object sender, EventArgs e)
+        {
+            imhaKontrol();
+        }
+        //Düşmanın imhasını kontrol eder
+        public bool imhaKontrol()
+        {
+            List<PictureBox> mermiList = new List<PictureBox>();
+            List<PictureBox> dusmanList = new List<PictureBox>();
+
+            // atılan mermi ve çıkan düşmanları listbox ekler
+            foreach (Control c in this.Controls)
+            {
+                if (c.Height == 12)
+                {
+                    mermiList.Add((PictureBox)c);
+                }
+                else if (c.Height == 40)
+                {
+                    dusmanList.Add((PictureBox)c);
+                }
+            }
+            for (int i = 0; i < dusmanList.Count; i++)
+            {
+                for (int j = 0; j < mermiList.Count; j++)
+                {
+                    if (yokEdildi(dusmanList[i], mermiList[j]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        
+        // Mermi konumu ile düşmanın konumunu eşleştirir. gereklı koşul sağlandığında iki cisimde kaybolur
+        public bool yokEdildi(PictureBox dusman, PictureBox mermi)
+        {
+            try
+            {               
+                if ((mermi.Location.Y >= dusman.Location.Y - 18 && mermi.Location.Y <= dusman.Location.Y + 40) && (mermi.Location.X >= dusman.Location.X && mermi.Location.X <= dusman.Location.X + 40))
+                {
+                    if (mermi.BackgroundImage.Width == resMermi.Width)
+                    {
+                        mermi.Dispose();
+                        dusman.Dispose();
+                    }
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return false;
+        }
+    }
+}
+
